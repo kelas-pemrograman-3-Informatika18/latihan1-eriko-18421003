e88d67a737a7a25e4150e87a7b062bd283d35d7e
erikozakaria <erikomedia123@gmail.com>
2020-03-26 11:39:15 +0700

    first commit

---
+++ src/router/routes.js
@@ -0,0 +1,27 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/LoginPage.vue') }
+    ]
+  },
+  {
+    path: '/home',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/LoginPage.vue') }
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
